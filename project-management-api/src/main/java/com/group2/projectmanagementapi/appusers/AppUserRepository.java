7931f289a41e88d3b07142e72500089e28622b5f
Imam Sobirin <imamsobirin34@gmail.com>
2023-06-04 12:40:32 +0000

    Revert "Merge branch 'imamdevs' into 'development'"
    This reverts merge request !3

--- project-management-api/src/main/java/com/group2/projectmanagementapi/appusers/AppUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group2.projectmanagementapi.appusers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long>{
-
-}
